3acc56da0f0e8827f9a459af87b7026842918659
Cody Bonsma <codybonsma@gmail.com>
2020-11-16 20:22:24 -0500

    added search functionality to search books

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
 
 const Search = () => {
-    return (
-        <div>
-            <h2>This will be the search page</h2>
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    API.findBook("Jung")
+      .then((response) => {
+        console.log(response.data.items);
+        setBook(response.data.items);
+      })
+      .catch((err) => {
+        if (err) throw err;
+      });
+  }, []);
+
+const handleChange = (e) => {
+    API.findBook(e)
+    .then((response) => {
+      console.log(response.data.items);
+      setBook(response.data.items);
+    })
+    .catch((err) => {
+      if (err) throw err;
+    });
+}
+
+  return (
+    <div className="container fluid">
+      <div className="row">
+        <div className="col">
+          <h2>This will be the search page</h2>
+          <div className="search-bar row">
+            <div className="col-sm-3" />
+            <div className="col-sm-6 search-div">
+              <input
+                type="text"
+                className="form-control input-search"
+                onChange={(e) => handleChange(e.target.value)}
+                placeholder="Search for employee"
+                aria-describedby="inputGroup-sizing-default"
+              />
+            </div>
+            <div className="col-sm-3" />
+          </div>
+          <table className="table table-dark">
+            <thead>
+              <tr>
+                <th scope="col">Image</th>
+                <th scope="col">Title</th>
+                <th scope="col">Author</th>
+                <th scope="col">Link</th>
+                {/* <th scope="col">Age</th> */}
+              </tr>
+            </thead>
+            <tbody>
+              {book.map((book) => {
+                return (
+                  <tr>
+                    <th scope="row">
+                      <img src={book.volumeInfo.imageLinks.smallThumbnail} />
+                    </th>
+                    <td key={book.volumeInfo.id}>{book.volumeInfo.title}</td>
+                    <td>{book.volumeInfo.authors}</td>
+                    <td>
+                      <a href="">link</a>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Search;
--- controller/bookController.js
+++ controller/bookController.js
@@ -3,15 +3,35 @@ const router = require("router");
 const db = require("../models");
 
 // route to get all books
-router.get("/", (req, res) => {
-    db.Book.find({}).then((resonse) => {
-        console.log(response);
-        res.json(reponse)
-    }).catch((err) =>{
-        res.status(500).json({
-            error: true,
-            data: null,
-            message: "Failed to retrieve all books."
-        })
+router.get("/api/books", (req, res) => {
+  db.Book.find({})
+    .then((resonse) => {
+      console.log(response);
+      res.json(reponse);
     })
+    .catch((err) => {
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Failed to retrieve all books.",
+      });
+    });
+});
+
+router.post("/api/books", (req, res) => {
+  console.log(req.body);
+  db.Book.create({})
+    .then((newBook) => {
+      console.log(newBook);
+    })
+    .then((err) => {
+      if (err) throw err;
+    });
+});
+
+router.delete("/api/books/:id", (req, res) => {
+    console.log(req.body);
+    // create delete functionality
 })
+
+export default router;
